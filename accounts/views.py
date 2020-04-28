909d8462f530332f4c5fafbb8fd63f23cb94a92c
tungpham1029 <tungpham1029@gmail.com>
2020-04-28 19:41:14 +0700

    blog_accounts_restfw

---
+++ accounts/views.py
@@ -0,0 +1,65 @@
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+
+from .decorators import unauthenticated_user
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
+# Create your views here.
+
+
+@unauthenticated_user
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account '+ username +' been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'accounts/register.html', {'form':form})
+
+
+@login_required()
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'accounts/profile.html', context)
+
+
+@unauthenticated_user
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
